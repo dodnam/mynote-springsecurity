fac5bf85c916c4db25e3e00c736a3fbf4178af99
ghryu <dodnam@naver.com>
2023-02-03 14:38:53 +0900

    Spring Boot 초기 Starter 세팅

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.nam.mynotespringsecurity {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.nam.mynotespringsecurity to javafx.fxml;
-    exports com.nam.mynotespringsecurity;
-}
