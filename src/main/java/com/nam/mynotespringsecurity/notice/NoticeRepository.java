d338c34f10a0a99119c415f13c526d5424ce2906
ghryu <dodnam@naver.com>
2023-02-06 11:18:20 +0900

    공지사항 Notice
    조회, 저장, 삭제

---
+++ src/main/java/com/nam/mynotespringsecurity/notice/NoticeRepository.java
@@ -0,0 +1,6 @@
+package com.nam.mynotespringsecurity.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
