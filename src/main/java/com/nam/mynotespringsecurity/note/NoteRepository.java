70d80c894e5eb807213d95b06d328fbe2a42dd0f
ghryu <dodnam@naver.com>
2023-02-06 11:14:15 +0900

    Note
    조회, 저장, 삭제

---
+++ src/main/java/com/nam/mynotespringsecurity/note/NoteRepository.java
@@ -0,0 +1,13 @@
+package com.nam.mynotespringsecurity.note;
+
+import com.nam.mynotespringsecurity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findByUserOrderByIdDesc(User user);
+
+    Note findByIdAndUser(Long id, User user);
+}
